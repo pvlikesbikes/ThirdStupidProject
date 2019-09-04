75c375f60a24c534feeaabb5ce1115fcb352fa6f
pvlikesbikes <54409593+pvlikesbikes@users.noreply.github.com>
2019-09-04 09:15:28 -0600

    Initial commit to ThirdStupidProject

---
+++ src/third/controller/Controller.java
@@ -0,0 +1,15 @@
+package third.controller;
+
+public class Controller
+{
+
+	public void start()
+	{
+		System.out.println("Baby cat bear iguana");
+		System.out.println("Has four legs and sits");
+		System.out.println("And then runs away");
+		System.out.print("little booties on his feet");
+		System.out.print("He trips and bites his lip");
+		System.out.println("Type you filthy swine");
+	}
+}
---
+++ src/third/controller/Runner.java
@@ -0,0 +1,11 @@
+package third.controller;
+
+public class Runner
+{
+	public static void main (String [] args)
+	{
+		Controller app = new Controller ();
+		app.start();
+	}
+
+}
