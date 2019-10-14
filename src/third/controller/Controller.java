972f5de8af85b7bfce84f403b9e8c514d0a11548
pvlikesbikes <54409593+pvlikesbikes@users.noreply.github.com>
2019-10-14 08:16:40 -0600

    Made a list with a loop in it.

--- src/third/controller/Controller.java
+++ src/third/controller/Controller.java
@@ -1,5 +1,6 @@
 package third.controller;
 
+import java.util.ArrayList;
 //need to import popup bc its in a different package
 import third.view.Popup;
 
@@ -7,64 +8,78 @@ public class Controller
 {
 
 	private Popup view;
-	
+
 	public Controller()
 	{
 		view = new Popup();
 	}
-	
+
 	public void start()
-	{	
-		loopTest();
+	{
+		usingLists();
+		// loopTest();
 	}
-	
+
+	private void usingLists()
+	{
+		ArrayList<String> userInput = new ArrayList<String>();
+
+		String input = view.askQuestion("Type in some words or done to stop");
+		
+		while (!input.equalsIgnoreCase("done"))
+		{
+			userInput.add(input);
+			input = view.askQuestion("Keep typing or type done.");
+		}
+	}
+
 	private void loopTest()
 	{
-		
-		//simon says do something and only stop when he says so
-		
-		//define the variable
+
+		// simon says do something and only stop when he says so
+
+		// define the variable
 		boolean isFinished = false;
 		view.displayMessage("Beans boy");
 		int count = 0;
-		
-		//test the variable
+
+		// test the variable
 		while (!isFinished)
 		{
-			//do things and dont stop here
+			// do things and dont stop here
 			System.out.println("Beans and stuff" + count);
-			
-			//view.displayMessage(count + "");
+
+			// view.displayMessage(count + "");
 			String response = view.askQuestion("Hi, beans?");
 			view.displayMessage(response + "?");
-			count ++;
-					
+			count++;
+
 			if (count > 130)
 			{
-				//make a stopping point
+				// make a stopping point
 				isFinished = true;
 			}
 		}
 		System.out.println("Beans and things at: " + count);
-		
+
 		while (count > 0)
 		{
 			System.out.println(count);
 			count--;
-			
+
 		}
-		
+
 		int special = 1;
 		do
 		{
 			System.out.println(special);
 			special--;
-		}while (special > 0);
-		
+		} while (special > 0);
+
 		for (int spot = 0; spot < 100; spot++)
 		{
 			System.out.println("Beans are: " + spot);
 		}
-		//spot is not visible anymore
+		// spot is not visible anymore
 	}
 }
